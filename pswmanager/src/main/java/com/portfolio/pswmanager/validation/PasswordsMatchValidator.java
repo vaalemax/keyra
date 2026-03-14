d3d4c7de824e31bed753db58d18f9a1b54434551
vaalemax <valemaxpersonal@gmail.com>
2026-03-14 23:28:44 +0100

    pulizia folder validation

--- pswmanager/src/main/java/com/portfolio/pswmanager/validation/PasswordsMatchValidator.java
+++ pswmanager/src/main/java/com/portfolio/pswmanager/validation/PasswordsMatchValidator.java
@@ -4,9 +4,6 @@ import com.portfolio.pswmanager.model.dto.RegisterRequest;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-/**
- * Validator that checks password == confirmPassword.
- */
 public class PasswordsMatchValidator implements ConstraintValidator<PasswordsMatch, RegisterRequest> {
 
     @Override
