d9a9610f186a9363db539a27e37b2f75a7b9e2f6
vaalemax <valemaxpersonal@gmail.com>
2026-07-25 13:26:23 +0200

    PasswordGenerationResponse converted to record

--- pswmanager/src/main/java/com/portfolio/pswmanager/model/dto/PasswordGenerationResponse.java
+++ pswmanager/src/main/java/com/portfolio/pswmanager/model/dto/PasswordGenerationResponse.java
@@ -1,15 +1,3 @@
 package com.portfolio.pswmanager.model.dto;
 
-import lombok.Getter;
-
-@Getter
-public class PasswordGenerationResponse {
-    private final String password;
-    private final int length;
-
-    public PasswordGenerationResponse(String password, int length) {
-        this.password = password;
-        this.length = length;
-    }
-
-}
+public record PasswordGenerationResponse(String password, int length) {}
