8e952af90c55719e8d30d0b53c838ce506a45851
vaalemax <valemaxpersonal@gmail.com>
2026-07-25 13:41:40 +0200

    throws Exception

--- pswmanager/src/main/java/com/portfolio/pswmanager/config/SecurityConfig.java
+++ pswmanager/src/main/java/com/portfolio/pswmanager/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
     private final RateLimitingFilter rateLimitingFilter;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http){
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .addFilterBefore(rateLimitingFilter, UsernamePasswordAuthenticationFilter.class)
 
